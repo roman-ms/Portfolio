4bece4c09806119ca9f1d20549c26bb638992de1
Roman <49215713+roman-ms@users.noreply.github.com>
2023-06-28 14:52:37 -0500

    Imported my 3D model

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,7 +5,7 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./desktop_pc/scene.gltf");
+  const computer = useGLTF("./desktop_pc/3D.glb");
 
   return (
     <mesh>
@@ -18,12 +18,12 @@ const Computers = ({ isMobile }) => {
         castShadow
         shadow-mapSize={1024}
       />
-      <pointLight intensity={1} />
+      <pointLight intensity={0} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
+        scale={isMobile ? 4 : 3.5}
+        position={isMobile ? [0, -2, -1] : [-1.5, -4, -1]}
+        rotation={[0, 1.2, 0]}
       />
     </mesh>
   );
