4dcabd94fbd6f16369da55e81236e288f3b5cfe7
Roman <49215713+roman-ms@users.noreply.github.com>
2023-07-03 17:33:23 -0500

    added cat logo

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,6 +21,7 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
+import caterpillar from "./company/caterpillar.png";
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
